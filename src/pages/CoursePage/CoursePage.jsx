1be3077750d90df2fb88626118d01e8587a3deb0
TryNoRage <shaiturovtr@gmail.com>
2023-03-17 21:32:48 +0200

    Add VideoHSL component

--- src/pages/CoursePage/CoursePage.jsx
+++ src/pages/CoursePage/CoursePage.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, {useState, useEffect} from "react";
+import VideoHLS from "../../components/VideoHLS/VideoHLS";
+import { useParams } from "react-router-dom";
+import useFetch from "../../customHooks/useFetch";
 
 function CoursePage() {
-  const {id} = useParams()
+  const { id } = useParams();
+  const [course, isLoading ]  = useFetch(id);
+ 
   return (
-    <div>CoursePage {id}</div>
-  )
+    <>
+      {isLoading? "rre3" : <VideoHLS videoLink={course.meta.courseVideoPreview.link} />}
+    </>
+  );
 }
 
-export default CoursePage
+export default CoursePage;
