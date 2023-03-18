115a4b1c832620a07e9f09272d6ab65c4cbb091d
TryNoRage <shaiturovtr@gmail.com>
2023-03-18 16:30:33 +0200

    Add compnent VideoOnHover

--- src/components/CourseItem/CourseItem.jsx
+++ src/components/CourseItem/CourseItem.jsx
@@ -1,23 +1,28 @@
-import { Card } from "@mui/material";
+import { Card, Rating } from "@mui/material";
 import React, { useRef } from "react";
 import ListOfSkills from "../ListOfSkills/ListOfSkills";
 import "./CourseItem.css";
+import VideoOnHover from "../VideoOnHover/VideoOnHover";
 
 function CourseItem({ course, handleChildClick }) {
   return (
     <>
       <Card id="wrapper" onClick={() => handleChildClick(course.id)}>
         <div id="head-content">
-          <img
-            src={`${course.previewImageLink}/cover.webp`}
-            alt={course.title}
-            loading="lazy"
+          <VideoOnHover
+            videoLink={course.meta.courseVideoPreview.link}
+            videoTitle={course.title}
+            previewImage={`${course.previewImageLink}/cover.webp`}
           />
-          <h1>{course.title}</h1>
+          <h1 id="title-course">{course.title}</h1>
         </div>
         <div id="body-content">
           <p> {course.description}</p>
-          <p>Rating: {course.rating}</p>
+          <div id="rating">
+            <p>Rating:</p>
+            <Rating precision={0.1} value={course.rating} readOnly />
+          </div>
+
           <p>Lessons: {course.lessonsCount}</p>
         </div>
         <ListOfSkills skills={course.meta.skills || []} />
